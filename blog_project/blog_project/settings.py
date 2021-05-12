3f79ab3c98ca5091f77ef2ad52e09c26b6d0d0d9
vladimir <bloodoof1234@gmail.com>
2021-05-13 01:58:30 +0300

    Create user

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'accounts.app.AccountConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
